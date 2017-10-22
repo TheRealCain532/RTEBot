011911c30bfd9311d989dc22e0793ea24e48701a
Brandon <Brandon@DESKTOP-P9MC04D>
2017-10-21 19:20:00 -0700

    UPDATE

--- RTEBot/Games/MGS_PSX.cs
+++ RTEBot/Games/MGS_PSX.cs
@@ -15,7 +15,8 @@ namespace RTEBot.Games
 
         public Extension Extension { get { return new Extension(SelectAPI.ControlConsole); } }
         public CCAPI CCAPI { get { return new CCAPI(); } }
-
+        System.Timers.Timer aTimer;
+        bool infammo = false;
         public async void SendMessage(string input)
         {
             await Context.Channel.SendMessageAsync(input);
@@ -23,6 +24,45 @@ namespace RTEBot.Games
         public short SwitchInt16(short input)
         {
             return (short)((input << 8) + (input >> 8));
+        }
+        public enum current : int //0x38291C
+        {
+            non = 0xFFFF,
+            socom = 0x0000,
+            famas = 0x0100,
+            grenade = 0x0200,
+            nikita = 0x0300,
+            stinger = 0x0400,
+            claymore = 0x0500,
+            c4 = 0x0600,
+            stng = 0x0700,
+            chg = 0x0800,
+            psg1 = 0x0900
+        }
+        [Command("InfAmmo")]
+        public async Task _InfAmmo()
+        {
+            infammo = !infammo;
+            Console.WriteLine(infammo.ToString());
+            while (infammo)
+            {
+                int _current = Extension.ReadInt16(0x38291C);
+                switch (_current)
+                {
+                    case (int)current.socom: Giveth(MGS.Socom); break;
+                    case (int)current.famas: Giveth(MGS.Famas); break;
+                    case (int)current.grenade: Giveth(MGS.Grenade); break;
+                    case (int)current.nikita: Giveth(MGS.Nikita); break;
+                    case (int)current.stinger: Giveth(MGS.Stinger); break;
+                    case (int)current.claymore: Giveth(MGS.Claymore); break;
+                    case (int)current.c4: Giveth(MGS.C4); break;
+                    case (int)current.stng: Giveth(MGS.Stun_G); break;
+                    case (int)current.chg: Giveth(MGS.Chaff_G); break;
+                    case (int)current.psg1: Giveth(MGS.PSG1); break;
+                    case (int)current.non: infammo = false; break;
+                }
+                Extension.WriteBytes(0x379553, new byte[] { 0x55, 0x55 });
+            }
         }
         public enum MGS : uint
         {
@@ -64,172 +104,113 @@ namespace RTEBot.Games
         }
 
 
-        public void Giveeth(MGS address, string Weapon)
+        public void Giveth(MGS address, string Weapon)
         {
             Extension.WriteBytes((uint)address, new byte[] { 0x14, 0x00 });
             SendMessage(string.Format("{0}, Given! Thanks {1}!!", Weapon, Context.User.Mention));
         }
+        public void Giveth(MGS address)
+        {
+            Extension.WriteBytes((uint)address, new byte[] { 0x14, 0x00 });
+        }
         public void Taketh(MGS address, string Weapon)
         {
             Extension.WriteBytes((uint)address, new byte[] { 0xFF, 0xFF});
-            SendMessage(string.Format("{{1} Took your {0}!", Weapon, Context.User.Mention));
+            SendMessage(string.Format("{1} Took your {0}!", Weapon, Context.User.Mention));
         }
+
         [Command("Give")]
-        public async Task _GiveWeapon(string Weap)
+        public async Task _Give(string Weap)
         {//Next time try ToLower or ToUpper :P dingus
             if (Variables.IsConnected)
             {
+                string stuff = "";
+                stuff = Weap.Replace(" ", "_").ToLower();
+                Console.WriteLine(stuff);
                 byte[] give = { 0x14, 0x00 };
-                switch (Weap)
+                switch (stuff)
                 {
-                    case "Socom": Giveeth(MGS.Socom, Weap); break;
-                    case "socom": Giveeth(MGS.Socom, Weap); break;
-                    case "Famas": Giveeth(MGS.Famas, Weap); break;
-                    case "famas": Giveeth(MGS.Famas, Weap); break;
-                    case "Grenade": Giveeth(MGS.Grenade, Weap); break;
-                    case "grenade": Giveeth(MGS.Grenade, Weap); break;
-                    case "Nikita": Giveeth(MGS.Nikita, Weap); break;
-                    case "nikita": Giveeth(MGS.Nikita, Weap); break;
-                    case "Stinger": Giveeth(MGS.Stinger, Weap); break;
-                    case "stinger": Giveeth(MGS.Stinger, Weap); break;
-                    case "Claymore": Giveeth(MGS.Claymore, Weap); break;
-                    case "claymore": Giveeth(MGS.Claymore, Weap); break;
-                    case "C4": Giveeth(MGS.C4, Weap); break;
-                    case "c4": Giveeth(MGS.C4, Weap); break;
-                    case "StunGrenade": Giveeth(MGS.Stun_G, Weap); break;
-                    case "stungrenade": Giveeth(MGS.Stun_G, Weap); break;
-                    case "ChaffGrenade": Giveeth(MGS.Chaff_G, Weap); break;
-                    case "chaffgrenade": Giveeth(MGS.Chaff_G, Weap); break;
-                    case "PSG1": Giveeth(MGS.PSG1, Weap); break;
-                    case "psg1": Giveeth(MGS.PSG1, Weap); break;
+                    case "socom": Giveth(MGS.Socom, Weap); break;
+                    case "famas": Giveth(MGS.Famas, Weap); break;
+                    case "grenade": Giveth(MGS.Grenade, Weap); break;
+                    case "nikita": Giveth(MGS.Nikita, Weap); break;
+                    case "stinger": Giveth(MGS.Stinger, Weap); break;
+                    case "claymore": Giveth(MGS.Claymore, Weap); break;
+                    case "c4": Giveth(MGS.C4, Weap); break;
+                    case "stun_grenade": Giveth(MGS.Stun_G, Weap); break;
+                    case "chaff_grenade": Giveth(MGS.Chaff_G, Weap); break;
+                    case "psg1": Giveth(MGS.PSG1, Weap); break;
 
-                    case "Cigs": Giveeth(MGS.Cig, Weap); break;
-                    case "cigs": Giveeth(MGS.Cig, Weap); break;
-                    case "Scope": Giveeth(MGS.Scope, Weap); break;
-                    case "scope": Giveeth(MGS.Scope, Weap); break;
-                    case "CardboardBoxA": Giveeth(MGS.CardboardBoxA, Weap); break;
-                    case "BoxA": Giveeth(MGS.CardboardBoxA, Weap); break;
-                    case "CardboardBoxB": Giveeth(MGS.CardboardBoxB, Weap); break;
-                    case "BoxB": Giveeth(MGS.CardboardBoxB, Weap); break;
-                    case "CardboardBoxC": Giveeth(MGS.CardboardBoxC, Weap); break;
-                    case "BoxC": Giveeth(MGS.CardboardBoxC, Weap); break;
-                    case "NightVisionGoggles": Giveeth(MGS.NVG, Weap); break;
-                    case "NVG": Giveeth(MGS.NVG, Weap); break;
-                    case "ThermalGoggles": Giveeth(MGS.ThermG, Weap); break;
-                    case "ThermG": Giveeth(MGS.ThermG, Weap); break;
-                    case "GasMask": Giveeth(MGS.GasM, Weap); break;
-                    case "GMask": Giveeth(MGS.GasM, Weap); break;
-                    case "BodyArmor": Giveeth(MGS.BodyArmor, Weap); break;
-                    case "BArmor": Giveeth(MGS.BodyArmor, Weap); break;
-                    case "Ketchup": Giveeth(MGS.Ketchup, Weap); break;
-                    case "kethcup": Giveeth(MGS.Ketchup, Weap); break;
-                    case "Stealth": Giveeth(MGS.Stealth, Weap); break;
-                    case "Camo": Giveeth(MGS.Stealth, Weap); break;
-                    case "Bandana": Giveeth(MGS.Bandanna, Weap); break;
-                    case "bandana": Giveeth(MGS.Bandanna, Weap); break;
-                    case "Camera": Giveeth(MGS.Camera, Weap); break;
-                    case "camera": Giveeth(MGS.Camera, Weap); break;
-                    case "Ration": Extension.WriteBytes((uint)MGS.Ration, new byte[] { 0x04, 0x00 }); break;//Giveeth(MGS.Ration, Weap); break;
-                    case "ration": Extension.WriteBytes((uint)MGS.Ration, new byte[] { 0x04, 0x00 }); break;
-                    case "Medicine": Giveeth(MGS.Medicine, Weap); break;
-                    case "medicine": Giveeth(MGS.Medicine, Weap); break;
-                    case "Diazepam": Giveeth(MGS.NVG, Weap); break;
-                    case "diazepam": Giveeth(MGS.NVG, Weap); break;
-                    case "Pal": Giveeth(MGS.PalKey, Weap); break;
-                    case "pal": Giveeth(MGS.PalKey, Weap); break;
-                    case "MineDetector": Giveeth(MGS.MineDetector, Weap); break;
-                    case "MineD": Giveeth(MGS.MineDetector, Weap); break;
-                    case "Rope": Giveeth(MGS.Rope, Weap); break;
-                    case "rope": Giveeth(MGS.Rope, Weap); break;
-                    case "Handkerchief": Giveeth(MGS.Handkerchief, Weap); break;
-                    case "handkerchief": Giveeth(MGS.Handkerchief, Weap); break;
-                    case "Suppressor": Giveeth(MGS.Suppressor, Weap); break;
-                    case "suppressor": Giveeth(MGS.Suppressor, Weap); break;
-
-                    case "PAL": Giveeth(MGS.PalKey, Weap); break;//Giveeth(MGS.PalKey, Weap); break;
-                    case "KeyCard": Giveeth(MGS.KeyCard, Weap); break;
-                    case "Key": Giveeth(MGS.KeyCard, Weap); break;
-                    case "Disc": Giveeth(MGS.Disc, Weap); break;
-                    case "disc": Giveeth(MGS.Disc, Weap); break;
+                    case "cigs": Giveth(MGS.Cig, Weap); break;
+                    case "scope": Giveth(MGS.Scope, Weap); break;
+                    case "cardboard_box_a": Giveth(MGS.CardboardBoxA, Weap); break;
+                    case "cardboard_box_b": Giveth(MGS.CardboardBoxB, Weap); break;
+                    case "cardboard_box_c": Giveth(MGS.CardboardBoxC, Weap); break;
+                    case "night_vision_goggles": Giveth(MGS.NVG, Weap); break;
+                    case "thermal_goggles": Giveth(MGS.ThermG, Weap); break;
+                    case "gas_mask": Giveth(MGS.GasM, Weap); break;
+                    case "body_armor": Giveth(MGS.BodyArmor, Weap); break;
+                    case "ketchup": Giveth(MGS.Ketchup, Weap); break;
+                    case "stealth": Giveth(MGS.Stealth, Weap); break;
+                    case "bandana": Giveth(MGS.Bandanna, Weap); break;
+                    case "camera": Giveth(MGS.Camera, Weap); break;
+                    case "ration": Giveth(MGS.Ration, Weap); break;
+                    case "medicine": Giveth(MGS.Medicine, Weap); break;
+                    case "diazepam": Giveth(MGS.NVG, Weap); break;
+                    case "pal": Giveth(MGS.PalKey, Weap); break;
+                    case "key": Giveth(MGS.KeyCard, Weap); break;
+                    case "mine_detector": Giveth(MGS.MineDetector, Weap); break;
+                    case "disc": Giveth(MGS.Disc, Weap); break;
+                    case "rope": Giveth(MGS.Rope, Weap); break;
+                    case "handkerchief": Giveth(MGS.Handkerchief, Weap); break;
+                    case "suppressor": Giveth(MGS.Suppressor, Weap); break;
                         //case "All": foreach (var item in Enum.GetValues(typeof(MGS))) Extension.WriteBytes((uint)item, give); SendMessage(string.Format("{1} Gave you Everything!!", Weap, Context.User.Mention)); break;
-                        //case "all": foreach (var item in Enum.GetValues(typeof(MGS))) Extension.WriteBytes((uint)item, give); SendMessage(string.Format("{1} Gave you Everything!!", Weap, Context.User.Mention)); break;
+                        case "all": foreach (var item in Enum.GetValues(typeof(MGS))) Extension.WriteBytes((uint)item, give); SendMessage(string.Format("{1} Gave you Everything!!", Weap, Context.User.Mention)); break;
                 }
             }
         }
         [Command("Take")]
-        public async Task _TakeWeapon(string Weap)
+        public async Task _Take(string Weap)
         {
+            string stuff = "";
+            stuff = Weap.Replace(" ", "_").ToLower();
             if (Variables.IsConnected)
             {
                 byte[] take = { 0xFF, 0xFF };
-                switch (Weap)
+                switch (stuff)
                 {
-                    case "Socom": Taketh(MGS.Socom, Weap); break;
                     case "socom": Taketh(MGS.Socom, Weap); break;
-                    case "Famas": Taketh(MGS.Famas, Weap); break;
                     case "famas": Taketh(MGS.Famas, Weap); break;
-                    case "Grenade": Taketh(MGS.Grenade, Weap); break;
                     case "grenade": Taketh(MGS.Grenade, Weap); break;
-                    case "Nikita": Taketh(MGS.Nikita, Weap); break;
                     case "nikita": Taketh(MGS.Nikita, Weap); break;
-                    case "Stinger": Taketh(MGS.Stinger, Weap); break;
                     case "stinger": Taketh(MGS.Stinger, Weap); break;
-                    case "Claymore": Taketh(MGS.Claymore, Weap); break;
                     case "claymore": Taketh(MGS.Claymore, Weap); break;
-                    case "C4": Taketh(MGS.C4, Weap); break;
                     case "c4": Taketh(MGS.C4, Weap); break;
-                    case "Stun Grenade": Taketh(MGS.Stun_G, Weap); break;
-                    case "stun grenade": Taketh(MGS.Stun_G, Weap); break;
-                    case "Chaff Grenade": Taketh(MGS.Chaff_G, Weap); break;
-                    case "chaff grenade": Taketh(MGS.Chaff_G, Weap); break;
-                    case "PSG1": Taketh(MGS.PSG1, Weap); break;
+                    case "stun_grenade": Taketh(MGS.Stun_G, Weap); break;
+                    case "chaff_grenade": Taketh(MGS.Chaff_G, Weap); break;
                     case "psg1": Taketh(MGS.PSG1, Weap); break;
-
-                    case "Cigs": Taketh(MGS.Cig, Weap); break;
                     case "cigs": Taketh(MGS.Cig, Weap); break;
-                    case "Scope": Taketh(MGS.Scope, Weap); break;
                     case "scope": Taketh(MGS.Scope, Weap); break;
-                    case "CardboardBoxA": Taketh(MGS.CardboardBoxA, Weap); break;
-                    case "BoxA": Taketh(MGS.CardboardBoxA, Weap); break;
-                    case "CardboardBoxB": Taketh(MGS.CardboardBoxB, Weap); break;
-                    case "BoxB": Taketh(MGS.CardboardBoxB, Weap); break;
-                    case "CardboardBoxC": Taketh(MGS.CardboardBoxC, Weap); break;
-                    case "BoxC": Taketh(MGS.CardboardBoxC, Weap); break;
-                    case "NightVisionGoggles": Taketh(MGS.NVG, Weap); break;
-                    case "NVG": Taketh(MGS.NVG, Weap); break;
-                    case "ThermalGoggles": Taketh(MGS.ThermG, Weap); break;
-                    case "ThermG": Taketh(MGS.ThermG, Weap); break;
-                    case "Gas Mask": Taketh(MGS.GasM, Weap); break;
-                    case "GMask": Taketh(MGS.GasM, Weap); break;
-                    case "BodyArmor": Taketh(MGS.BodyArmor, Weap); break;
-                    case "BArmor": Taketh(MGS.BodyArmor, Weap); break;
-                    case "Ketchup": Taketh(MGS.Ketchup, Weap); break;
-                    case "kethcup": Taketh(MGS.Ketchup, Weap); break;
-                    case "Stealth": Taketh(MGS.Stealth, Weap); break;
-                    case "Camo": Taketh(MGS.Stealth, Weap); break;
-                    case "Bandana": Taketh(MGS.Bandanna, Weap); break;
+                    case "cardboard_box_a" : Taketh(MGS.CardboardBoxA, Weap); break;
+                    case "cardboard_box_b": Taketh(MGS.CardboardBoxB, Weap); break;
+                    case "cardboard_box_c": Taketh(MGS.CardboardBoxC, Weap); break;
+                    case "night_vision_goggles": Taketh(MGS.NVG, Weap); break;
+                    case "thermal_goggles": Taketh(MGS.ThermG, Weap); break;
+                    case "gas_mask": Taketh(MGS.GasM, Weap); break;
+                    case "body_armor": Taketh(MGS.BodyArmor, Weap); break;
+                    case "ketchup": Taketh(MGS.Ketchup, Weap); break;
+                    case "stealth": Taketh(MGS.Stealth, Weap); break;
                     case "bandana": Taketh(MGS.Bandanna, Weap); break;
-                    case "Camera": Taketh(MGS.Camera, Weap); break;
                     case "camera": Taketh(MGS.Camera, Weap); break;
-                    case "Ration": Taketh(MGS.Ration, Weap); break;
                     case "ration": Taketh(MGS.Ration, Weap); break;
-                    case "Medicine": Taketh(MGS.Medicine, Weap); break;
                     case "medicine": Taketh(MGS.Medicine, Weap); break;
-                    case "Diazepam": Taketh(MGS.NVG, Weap); break;
                     case "diazepam": Taketh(MGS.NVG, Weap); break;
-                    case "PAL": Taketh(MGS.PalKey, Weap); break;
-                    case "Pal Key": Taketh(MGS.PalKey, Weap); break;
-                    case "KeyCard": Taketh(MGS.KeyCard, Weap); break;
-                    case "Key": Taketh(MGS.KeyCard, Weap); break;
-                    case "Mine Detector": Taketh(MGS.MineDetector, Weap); break;
-                    case "Mine D": Taketh(MGS.MineDetector, Weap); break;
-                    case "Disc": Taketh(MGS.Disc, Weap); break;
+                    case "pal": Taketh(MGS.PalKey, Weap); break;
+                    case "key": Taketh(MGS.KeyCard, Weap); break;
+                    case "mine_detector": Taketh(MGS.MineDetector, Weap); break;
                     case "disc": Taketh(MGS.Disc, Weap); break;
-                    case "Rope": Taketh(MGS.Rope, Weap); break;
                     case "rope": Taketh(MGS.Rope, Weap); break;
-                    case "Handkerchief": Taketh(MGS.Handkerchief, Weap); break;
                     case "handkerchief": Taketh(MGS.Handkerchief, Weap); break;
-                    case "Suppressor": Taketh(MGS.Suppressor, Weap); break;
                     case "suppressor": Taketh(MGS.Suppressor, Weap); break;
                     case "All": foreach (var item in Enum.GetValues(typeof(MGS))) Extension.WriteBytes((uint)item, take); SendMessage(string.Format("{1} Took EVERYTHING!!", Weap, Context.User.Mention)); break;
                     case "all": foreach (var item in Enum.GetValues(typeof(MGS))) Extension.WriteBytes((uint)item, take); SendMessage(string.Format("{1} Took EVERYTHING!!", Weap, Context.User.Mention)); break;
---
+++ RTEBot/Modules/Core.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Webhook;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace RTEBot.Modules
+{
+    public class Core : ModuleBase<SocketCommandContext>
+    {
+        [Command("CleanHouse", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public async Task Prune(int count)
+        {
+            await Prune(Context.Channel, count);
+        }
+
+
+
+        [Command("CleanHouse", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public async Task Prune(IMessageChannel channel, int count)
+        {
+            var messages = await channel.GetMessagesAsync(count).Flatten();
+            int messageCount = messages.Count();
+            this.Context.Channel.DeleteMessagesAsync(messages);
+            IUserMessage message = null;
+            if (messageCount == 1)
+                message = await channel.SendMessageAsync("Deleted 1 message");
+            else
+                message = await channel.SendMessageAsync($"Deleted {messageCount} messages.");
+
+            await Task.Delay(2000);
+            await message.DeleteAsync();
+        }
+        [Command("CleanHouse", RunMode = RunMode.Async)]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public Task Prune() => ReplyAsync("'!CleanHouse {message count}'");
+    }
+}
